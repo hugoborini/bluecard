6a4af6c406773c6522a9c02ab557387bf12d81d2
Hgo0123 <hcordillot@gmail.com>
2019-12-18 13:42:04 +0100

    add condition on js input

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,3 @@
-
 let input = document.querySelectorAll("input");
 let name_txt = document.querySelector(".card__holder");
 let card_nb_txt = document.querySelector(".card__number");
@@ -8,20 +7,39 @@ let name = input[0];
 let card_nb = input[1];
 let exp = input[2];
 
+let name_default = "John Doe";
+let nb_default = "42 42 42 42 42";
+let exp_default = "10/22";
+
 name.addEventListener("keyup", function(event) {
   event.preventDefault();
-  name_txt.innerHTML =
-    "<p> <span>card holder name</span><br />" + name.value + "</p>";
+  if (name.value.length === 0) {
+    name_txt.innerHTML =
+      "<p> <span>card holder name</span><br />" + name_default + "</p>";
+  } else {
+    name_txt.innerHTML =
+      "<p> <span>card holder name</span><br />" + name.value + "</p>";
+  }
 });
 
 card_nb.addEventListener("keyup", function(event) {
   event.preventDefault();
-  card_nb_txt.innerHTML =
-    "<p> <span>card number</span> <br />" + card_nb.value + "</p>";
+  if (card_nb.value.length === 0) {
+    card_nb_txt.innerHTML =
+      "<p> <span>card number</span> <br />" + nb_default + "</p>";
+  } else {
+    card_nb_txt.innerHTML =
+      "<p> <span>card number</span> <br />" + card_nb.value + "</p>";
+  }
 });
 
 exp.addEventListener("keyup", function(event) {
   event.preventDefault();
-  exp_txt.innerHTML =
-    "<p> <span>card number</span> <br />" + exp.value + "</p>";
+  if (exp.value.length === 0) {
+    exp_txt.innerHTML =
+      "<p> <span>card number</span> <br />" + exp_default + "</p>";
+  } else {
+    exp_txt.innerHTML =
+      "<p> <span>card number</span> <br />" + exp.value + "</p>";
+  }
 });
